2192ccf904de7b643db34f4509655bb290d01e96
V4refT5f <V4refT5f@gmail.com>
2025-04-06 01:12:53 +0800

    lll

--- joyrun.c
+++ joyrun.c
@@ -14,6 +14,8 @@ DayIndex get_today_dayIndex() { return time(NULL) / (60 * 60 * 24); }
 Day query_day_info(DayIndex index) {
 	Day result = {0, 0, 0, 0, 1970};
 	
+	result.weekday = (index + 7000000 + 3) % WD_LEN;
+	
 	int yearOffset = 0;
 	int addupDays = query_days_in_year(1970);
 	if (index >= 0) {
@@ -24,19 +26,19 @@ Day query_day_info(DayIndex index) {
 	} else {
 		yearOffset --;
 		addupDays = query_days_in_year(1969);
-		while (addupDays <= -index) {
+		while (addupDays < -index) {
 			yearOffset --;
 			addupDays += query_days_in_year(1970 + yearOffset);
 		}
 	} 
 	result.year = 1970 + yearOffset;
-	printf("yearOffset: %d, addupDays: %d.\n", yearOffset, addupDays);
-	
+	// printf("yearOffset: %d, addupDays: %d.\n", yearOffset, addupDays);
+
+	int f = 0;
+	if (index < 0) { index = - index - 1; f = 1; }
 	int dayPozInYear =
 		index - (addupDays - query_days_in_year(result.year));
-		printf("dayPozInYear      : %d\n", dayPozInYear);
-		if (index < 0) {  }
-	printf("dayPozInYear (Neg): %d\n", dayPozInYear);
+		if (f) { dayPozInYear = query_days_in_year(result.year) - dayPozInYear - 1; }
 	MONTH monthOffset = JAN;
 	addupDays = query_days_in_month(result.year, JAN);
 	while (addupDays <= dayPozInYear) {
@@ -47,13 +49,13 @@ Day query_day_info(DayIndex index) {
 	result.monthday = dayPozInYear - (addupDays - query_days_in_month(result.year, monthOffset)) + 1;
 
 	result.index = index;
-	result.weekday = (index + 7000000 + 3) % WD_LEN;
 	return result;
 }
 
 
+
 int main([[maybe_unused]] const int argc,[[maybe_unused]] const char* argv[]) {
-/*
+
 	initscr();			// Sets up the memories needed by curses to eradicate its doubt of existance
 	cbreak();			// Disables line buffering like setbuf(stdout, NULL);.
 	noecho();			// Disables input echoing.
@@ -63,16 +65,26 @@ int main([[maybe_unused]] const int argc,[[maybe_unused]] const char* argv[]) {
 	refresh();			// Blits the buffer.
 	curs_set(0);		// Hides the cursor.
 
-	// Main loop here.
+	DayIndex today = get_today_dayIndex();
+	int thisPoz[2] = {5, 5};
+	int topleft[2] = {5, 5};
+
+	for (int i = 0; i < 100; i ++) {
+		Day thisDay = query_day_info(today + i);
+		if (thisDay.weekday == MON) { thisPoz[1] += 3; }
+		thisPoz[0] = topleft[0] + thisDay.weekday * 2;
+		mvprintw(thisPoz[0], thisPoz[1], "%d", thisDay.monthday);
+		if (thisDay.monthday == 1) mvprintw(topleft[0] - 1, thisPoz[1], "%s", MONTHS[thisDay.month]);
+	}
+
+	init_frame_control(60);
+	double wkT; double idT;
+	while (getch() != 'q') { frame_control(&wkT, &idT); }
+	end_frame_control();
 
 	endwin();			// Bye bye
 	putchar('\n');
 	return 0;
-*/	
-	Day day;
-	for (DayIndex d = -500; d < 500; d ++) {
-		day = query_day_info(d);
-		printf("Day %d, %s %-2d, %d, %s.\n", d, MONTHS[day.month], day.monthday, day.year, WEEKDAYS[day.weekday]);
-	}
-	return 0;
+
+	
 }
